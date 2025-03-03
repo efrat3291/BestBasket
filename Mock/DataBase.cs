f4c890268fb71b017ae2e68076d34fef4a1ed558
Efrat_Goldberg <efrat79071@gmail.com>
2025-03-03 11:25:16 +0200

    initial commit

---
+++ Mock/DataBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.InterFaces
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mock
+{
+    class DataBase:DbContext, IContext
+    {
+        
+    }
+}
